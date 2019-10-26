7821462bfec049742af703de269e4439bc3d86cf
Abhinandan-Kushwaha <kushabhi5@gmail.com>
2019-10-26 16:08:16 +0530

    Home Page Designed

--- App.js
+++ App.js
@@ -1,114 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
+  TouchableOpacity,
   StyleSheet,
-  ScrollView,
   View,
+  Image,
   Text,
-  StatusBar,
+  TextInput,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('./assets/logo.jpeg')}
+            style={styles.logoStyle}
+          />
+        </View>
+        <View style={styles.form}>
+          <View style={styles.row}>
+            <TextInput style={styles.formInput} placeholder="Email" />
+            <Icon color="#ff8100" name="envelope" size={20} />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          <View style={styles.row}>
+            <TextInput style={styles.formInput} placeholder="Email" />
+            <Icon color="#ff8100" name="key" size={20} />
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>SIGN IN</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <View style={styles.noButton}>
+              <Text>OR</Text>
+            </View>
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Sign In With Google</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <View style={styles.noButton}>
+              <Text>
+                Not a Member?<Text style={styles.linkText}>{' Sign Up '}</Text>
+                Now
+              </Text>
+            </View>
+          </View>
+          <View style={styles.buttonRow}>
+            <View style={styles.noButton}>
+              <Text style={styles.linkText}>Forgot Password?</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  logoContainer: {
+    marginVertical: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 90,
+    width: '100%',
   },
-  body: {
-    backgroundColor: Colors.white,
+  logoStyle: {
+    height: 80,
+    width: 180,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  form: {
+    flex: 1,
+    paddingHorizontal: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  formInput: {
+    flex: 1,
+    fontSize: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  row: {
+    flexDirection: 'row',
+    borderBottomColor: '#ff8100',
+    borderBottomWidth: 1,
+    marginHorizontal: 20,
+    paddingHorizontal: 4,
+    paddingBottom: 10,
+    marginBottom: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonRow: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    paddingHorizontal: 4,
+    paddingBottom: 10,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    width: '100%',
+    backgroundColor: '#ffa100',
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  noButton: {
+    width: '100%',
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  linkText: {
+    color: '#ff8100',
+    fontWeight: 'bold',
   },
 });
-
-export default App;
